66b6f558de33f71c123c435cb6ee8cd34c923a4e
Yuri Alves <yuripiresalves@gmail.com>
2022-11-01 14:54:11 -0300

    aula 1

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from 'native-base';
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="yellow.500" fontSize={24} fontFamily="heading">
+        SignIn
+      </Text>
+    </Center>
+  );
+}
